d9b0b2276ed7896bd388ea564a37a8719cbe889a
sclinis <sclinis@navent.com>
2022-06-27 18:23:15 -0300

    first version

--- src/main/java/com/navent/exercise/rest/PlayerController.java
+++ src/main/java/com/navent/exercise/rest/PlayerController.java
@@ -1,22 +1,11 @@
 package com.navent.exercise.rest;
 
-import com.navent.exercise.model.Player;
-import com.navent.exercise.repository.PlayerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("players")
 public class PlayerController {
-    @Autowired
-    private PlayerRepository playerRepository;
-
-    @RequestMapping
-    public List<Player> listPlayers() {
-        return playerRepository.getPlayerList();
-    }
 
 }
