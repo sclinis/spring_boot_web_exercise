5b96e92e67c4ff96181c5486aff2177629cf72d7
ReySalazar <salazarreymercedes@gmail.com>
2022-06-28 01:49:44 -0300

    Controller and Services player  implementation

--- src/main/java/com/navent/exercise/rest/PlayerController.java
+++ src/main/java/com/navent/exercise/rest/PlayerController.java
@@ -1,11 +1,31 @@
 package com.navent.exercise.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.navent.exercise.model.Player;
+import com.navent.exercise.services.PlayerServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("players")
 public class PlayerController {
+    @Autowired
+    PlayerServices playerServices;
 
+    @GetMapping("/")
+    public List<Player> getPlayers(){
+        return playerServices.getPlayers();
+    }
+
+    @GetMapping("/{name}")  // players/**playerName**
+    public List<Player> getPlayersByName(@PathVariable(value = "name") String name){
+        return playerServices.getPLayersByName(name);
+    }
+
+    @GetMapping("/player")  // players/player?id=**numberId**
+    public Player getPlayerById(@RequestParam Long id) {
+        return playerServices.getPlayerById(id);
+    }
 }
