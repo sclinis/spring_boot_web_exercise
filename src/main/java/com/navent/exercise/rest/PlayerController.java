86e1c5d02581fdde053274911cef72bf992a9233
epezzi <epezzi@navent.com>
2022-06-28 12:02:16 -0300

    implementada clase Services y modificada Controller

--- src/main/java/com/navent/exercise/rest/PlayerController.java
+++ src/main/java/com/navent/exercise/rest/PlayerController.java
@@ -1,11 +1,33 @@
 package com.navent.exercise.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.navent.exercise.model.Player;
+import com.navent.exercise.services.PlayerServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("players")
 public class PlayerController {
+
+    @Autowired
+    public PlayerServices playerServices;
+
+    @GetMapping
+    public List<Player> getPlayersList(){
+        return playerServices.getPlayerList();
+    }
+
+    @GetMapping("/{playerId}")
+    public Player getPlayerById(@PathVariable (value = "playerId", required = true) Long playerId){
+        return playerServices.getPlayerById(playerId);
+    }
+
+    @GetMapping("/filteredby")
+    public List<Player> getPlayerByName(@RequestParam (value = "name", required = true) String name){
+        return playerServices.getPlayersByName(name);
+    }
 
 }
