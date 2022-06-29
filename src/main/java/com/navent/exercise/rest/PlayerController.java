317c4809661bd9c0872c380f22178038144140c4
sclinis <sclinis@navent.com>
2022-06-29 09:41:07 -0300

    first version

--- src/main/java/com/navent/exercise/rest/PlayerController.java
+++ src/main/java/com/navent/exercise/rest/PlayerController.java
@@ -1,5 +1,8 @@
 package com.navent.exercise.rest;
 
+import com.navent.exercise.repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("players")
 public class PlayerController {
+    @Autowired
+    private PlayerRepository repository;
 
+    @PostMapping
+    public void test() {
+
+    }
 }
